374fee9d906d54dfaf7bf84fe16329a8914f1687
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-12 20:02:26 +0300

    added tslint ts-node-dev and started working on a dump cli

---
+++ erela.cli.ts
@@ -0,0 +1,18 @@
+import ora from 'ora'
+import 'colors'
+import { prompt, RawListQuestion } from 'inquirer'
+
+const listen = async () => {
+  console.log('Welcome to Erela Cli 👋👋'.cyan.bold)
+  const q01: RawListQuestion = {
+    type: 'rawlist',
+    message: 'What do you want to generate?',
+    choices: ['Controller', 'Model', 'Ma'],
+    name: 'generate',
+  }
+  prompt([q01]).then((ans) => {
+    console.log(ans)
+  })
+}
+
+listen()
