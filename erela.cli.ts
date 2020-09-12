b79ee506e8f188694d35d1700b43cd1bfe0da560
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-12 21:22:02 +0300

    Pipe Generation

--- erela.cli.ts
+++ erela.cli.ts
@@ -4,19 +4,39 @@ import { PlainObject } from 'types'
 import 'colors'
 import { prompt, RawListQuestion } from 'inquirer'
 
-const listen = async () => {
+enum choices {
+  Controller = 'Controller',
+  Entity = 'Entity',
+  Service = 'Service',
+}
+
+;(async () => {
   console.log('Welcome to Erela Cli 👋👋'.cyan.bold)
   const q01: RawListQuestion = {
     type: 'rawlist',
     message: 'What do you want to generate?',
-    choices: ['Controller', 'Entity', 'Interface', 'service'],
+    choices: Object.keys(choices),
     name: 'generate',
   }
   prompt([q01]).then((answer: PlainObject) => {
-    Object.keys(answer).forEach((question: string) => {
-      console.log(`${question}? --> `, answer[question])
-    })
+    const [ans] = Object.values(answer) as Array<choices>
+    pipeGeneration(ans)
   })
-}
+})()
 
-listen()
+const pipeGeneration = async (choice: choices) => {
+  switch (choice) {
+    case choices.Controller:
+      console.log('TODO generate a Controller'.blue)
+      break
+    case choices.Entity:
+      console.log('TODO generate a Entity'.yellow)
+      break
+    case choices.Service:
+      console.log('TODO generate a Service'.magenta)
+      break
+
+    default:
+      break
+  }
+}
