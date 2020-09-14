0d6e728675d9bc053f67d2cadb1f527099521f86
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-14 23:54:13 +0300

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
-// holder
+import 'reflect-metadata'
+import { createExpressServer } from 'routing-controllers'
+
+createExpressServer({
+  controllers: [__dirname + '/controllers/*.controller.ts'],
+}).listen(3030)
