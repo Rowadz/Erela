f86537730e194289985a12ba94b8feb9db99f2ca
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-25 19:32:20 +0300

    migrations

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -3,16 +3,16 @@ import { config } from 'dotenv'
 import 'reflect-metadata'
 import 'colors'
 config()
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env
+const { DB_HOST, DB_USER, DB_PASS, TYPEORM_DATABASE } = process.env
 
 export const connectWithDB = async (): Promise<Connection> => {
   const connection: Connection = await createConnection({
     type: 'sqlite',
-    database: `${DB_NAME}.db`,
+    database: TYPEORM_DATABASE as string,
     entities: [__dirname + '/**/*.entity.ts'],
   })
   await connection
-    .synchronize(true) // ! pass true to drop everything and create them again [ DO NOT DO IT ON PROD ]
+    .synchronize(false) // ! pass true to drop everything and create them again [ DO NOT DO IT ON PROD ]
     // tslint:disable-next-line: no-console
     .then(() => console.log('💾 Connected with DB 💾'.green.bold))
     .catch(() => console.error('Faild to sync with DB'.red.bold))
